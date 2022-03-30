093e145c23febd84ea078433227e8686f248f66a
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-30 15:50:25 +0000

    fuctions

--- first/script.js
+++ first/script.js
@@ -1,66 +1,77 @@
-var car = 100;
-do {
+// var car = 100;
+// do {
+//     console.log(car);
+//     car--;
+// } while (car > 0);
+/*var banku = 5;
+        var rice = 7;
+         let sum = banku + rice;
+        console.log(sum);*/
+//document.write(sum)
+// window.alert(sum);
+//  document.getElementById('kimi').innerHTML = sum;
+/*if (sum < banku) {
+    window.alert(sum);
+} else {
+    window.alert(banku);
+}
+
+if (1 > 5) {
+    console.log("hello");
+} else if (5 > 1) {
+    console.log("Hii");
+} else {
+    console.log("World");
+}*/
+/*var day = "Monday";
+switch (day) {
+    case "S": "Sunday";
+    break;
+    case "T": "Tuesday";
+    break;
+    case "W": "Wednesday";
+    break;
+    case "Th": "Thursday";
+    break;
+    case "M": "Monday";
+    break;
+    default:
+    "Saturday";
+    break;
+}*/
+/* var num = 1;
+ while (num <= 100) {
+     console.log(num);
+     num++;
+ }*/
+
+/*
+function sum(a,b) {
+    return a + b;
+}
+*/
+
+// Bitwise operators
+//6 | 6
+//6 & 6
+/*var car = 100;
+while (car > 0) {
     console.log(car);
     car--;
-} while (car > 0);
-/*var banku = 5;
-                                var rice = 7;
-                                let sum = banku + rice;
-                                console.log(sum);*/
-        //document.write(sum)
-        // window.alert(sum);
-        //  document.getElementById('kimi').innerHTML = sum;
-        /*if (sum < banku) {
-            window.alert(sum);
-        } else {
-            window.alert(banku);
-        }
+}*/
+// var num = 100;
+// do {
+//     console.log(num);
+//     num--;
+// } while (num > 0);
 
-        if (1 > 5) {
-            console.log("hello");
-        } else if (5 > 1) {
-            console.log("Hii");
-        } else {
-            console.log("World");
-        }*/
-        /*var day = "Monday";
-        switch (day) {
-            case "S": "Sunday";
-            break;
-            case "T": "Tuesday";
-            break;
-            case "W": "Wednesday";
-            break;
-            case "Th": "Thursday";
-            break;
-            case "M": "Monday";
-            break;
-            default:
-            "Saturday";
-            break;
-        }*/
-        /* var num = 1;
-         while (num <= 100) {
-             console.log(num);
-             num++;
-         }*/
+var arrayNam = [20, 40, 50.8, "hello"];
+var arrayName = [30, "hi", "world"];
+console.log(arrayNam.concat(arrayName));
 
-        /*
-        function sum(a,b) {
-            return a + b;
-        }
-        */
+// console.log(arrayNam.pop());
+// console.log(arrayNam.join());
 
-        // Bitwise operators
-        //6 | 6
-        //6 & 6
-        /*var car = 100;
-        while (car > 0) {
-            console.log(car);
-            car--;
-        }*/
-        var num = 100;
-        do {
-            console.log(num);
-            num--;
-        } while (num > 0);
+// console.log(arrayNam.toString());
+
+function isValidName() {}
