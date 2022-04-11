44a62fb5acc7541a7c2800ec2bf36efefd81405f
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-11 23:59:27 +0000

    git

--- first/script.js
+++ first/script.js
@@ -76,10 +76,10 @@ while (car > 0) {
 
 // function isValidName() {}
 
-const csv_to_array = (data, delimiter = ',', omitFirstRow = false) => data
-    .slice(omitFirstRow ? data.indexOf('\n') + 1 : 0)
-    .split('\n').map(v => v.split(delimiter));
+// const csv_to_array = (data, delimiter = ',', omitFirstRow = false) => data
+//     .slice(omitFirstRow ? data.indexOf('\n') + 1 : 0)
+//     .split('\n').map(v => v.split(delimiter));
 
-console.log(csv_to_array('a,b\nc,d'));
-console.log(csv_to_array('a;b\nc;d', ';'));
-console.log(csv_to_array('head1,head2\na,b\nc,d', ',', true));
+// console.log(csv_to_array('a,b\nc,d'));
+// console.log(csv_to_array('a;b\nc;d', ';'));
+// console.log(csv_to_array('head1,head2\na,b\nc,d', ',', true));
