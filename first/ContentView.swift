d5c62bcf18eab47e62391cef6c6c802f9a72232e
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-12 14:26:29 +0000

    push

--- first/ContentView.swift
+++ first/ContentView.swift
@@ -1,4 +1,7 @@
-func (hi) -> str {
-    print("Hello world")
-    
-}
+@main
+struct MyApp: App { // App is handling the `main` function
+  var body: some Scene {
+    WindowGroup {
+      SplashView()
+    }
+  }
