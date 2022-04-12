a8c48066e0771cdc9d970b3b4b07cd9e9a7487ef
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-12 02:14:39 +0000

    push

--- first/script.js
+++ first/script.js
@@ -162,16 +162,16 @@ while (car > 0) {
 // console.log(factors(15)); // [1,3,5,15]
 
 
-const vowels = ["a", "e", "i", "o", "u"];
+const vowel = ["a", "e", "i", "o", "u"];
 
-function check_vowels(str) {
+function checkVowels(str) {
     var vowel_arr = [];
 
     for (let letter of str.toLowerCase()) {
-        if (vowels.includes(letter)) {
+        if (vowel.includes(letter)) {
             vowel_arr.push(letter);
         }
     }
     return vowel_arr;
 }
-console.log(check_vowels("cricket")); // ['i','e']
+console.log(checkVowels("cricket")); // ['i','e']
