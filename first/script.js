8c584c94f9115e2681efbeaf671bd3fdc541fdc9
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-12 02:09:23 +0000

    push

--- first/script.js
+++ first/script.js
@@ -142,3 +142,21 @@ while (car > 0) {
 // }
 // console.log(checkEven(2));  
 // console.log(checkEven(3));
+
+function factors(num) {
+    var all_factors = [],
+        index;
+
+    for (index = 1; index <= Math.floor(Math.sqrt(num)); index += 1)
+        if (num % index === 0) {
+            all_factors.push(index);
+            if (num / index !== index)
+                all_factors.push(num / index);
+        }
+    all_factors.sort(function(x, y) {
+        return x - y;
+    }); // numeric sort
+    return all_factors;
+}
+
+console.log(factors(15)); // [1,3,5,15]
