d1520f4158ee5f58b35d4058dabec783a2e83252
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-19 14:10:15 +0000

    push

--- trial/main.py
+++ trial/main.py
@@ -1,5 +1,9 @@
-from ast import keyword
 
-
-keyword.kwlist
-print(keyword.kwlist)
+# recursive function
+def factorial(num):
+    if num == 1:
+        return num
+    else:
+         return num * factorial(num - 1)
+ 
+print(factorial(3))
