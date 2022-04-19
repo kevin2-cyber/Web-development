883553bad4df0d4a5c2bc8459d13bc67a99e7b47
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-19 16:03:39 +0000

    push

--- trial/qrcode.py
+++ trial/qrcode.py
@@ -1,9 +1,5 @@
 import io
 import qrcode
 
-qr = qrcode()
-qr.add_data("Hello, World")
-f = io.StringIO()
-qr.print_ascii(out=f)
-f.seek(0)
-print(f.read())
+img = qrcode.make("Hello World")
+img.save('hello.png')
