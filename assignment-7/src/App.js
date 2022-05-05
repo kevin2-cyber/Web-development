b5cf4f8559e34183d421169055d570345f9dea24
kevin2-cyber <keduful@st.ug.edu.gh>
2022-05-05 15:20:05 +0000

    name

--- assignment-7/src/App.js
+++ assignment-7/src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Interest from "./pages/Interest";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        div >
+        <
+        BrowserRouter >
+        <
+        NavBar / >
+        <
+        Routes >
+        <
+        Route path = "/"
+        element = { < Home / > }
+        /> <
+        Route path = "/about"
+        element = { < About / > }
+        /> <
+        Route path = "/blogs"
+        element = { < Contact / > }
+        /> <
+        Route path = "/settings"
+        element = { < Interest / > }
+        /> <
+        /Routes> <
+        /BrowserRouter> <
+        /div>
+    );
 }
 
 export default App;
